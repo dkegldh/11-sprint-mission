fe5bca0b9e5494943dbec11a0334d23d14c08fd7
apple <apple@MacBook-Pro.local>
2026-04-29 16:39:23 +0900

    chore: 환경별(dev/prod) 프로파일 분리, H2 테스트 DB 연동 및 운영 로깅 최적화

---
+++ src/main/resources/schema-h2.sql
@@ -0,0 +1,67 @@
+CREATE TABLE IF NOT EXISTS binary_contents
+(
+    id           UUID PRIMARY KEY,
+    created_at   TIMESTAMP WITH TIME ZONE NOT NULL,
+    file_name    VARCHAR(255)             NOT NULL,
+    size         BIGINT                   NOT NULL,
+    content_type VARCHAR(100)             NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS users
+(
+    id         UUID                     NOT NULL PRIMARY KEY,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE,
+    username   VARCHAR(50)              NOT NULL UNIQUE,
+    email      VARCHAR(100)             NOT NULL UNIQUE,
+    password   VARCHAR(60)              NOT NULL,
+    profile_id UUID UNIQUE              REFERENCES binary_contents (id) ON DELETE SET NULL
+);
+
+CREATE TABLE IF NOT EXISTS user_statuses
+(
+    id             UUID PRIMARY KEY,
+    created_at     TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at     TIMESTAMP WITH TIME ZONE,
+    user_id        UUID                     NOT NULL UNIQUE REFERENCES users (id) ON DELETE CASCADE,
+    last_active_at TIMESTAMP WITH TIME ZONE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS channels
+(
+    id              UUID PRIMARY KEY,
+    created_at      TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at      TIMESTAMP WITH TIME ZONE,
+    name            VARCHAR(100),
+    description     VARCHAR(500),
+    type            VARCHAR(10)              NOT NULL,
+    last_message_at TIMESTAMP WITH TIME ZONE
+);
+
+CREATE TABLE IF NOT EXISTS messages
+(
+    id         UUID PRIMARY KEY,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE,
+    content    TEXT,
+    channel_id UUID                     NOT NULL REFERENCES channels (id) ON DELETE CASCADE,
+    author_id  UUID                     REFERENCES users (id) ON DELETE SET NULL
+);
+
+CREATE TABLE IF NOT EXISTS read_statuses
+(
+    id           UUID PRIMARY KEY,
+    created_at   TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at   TIMESTAMP WITH TIME ZONE,
+    user_id      UUID                     NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+    channel_id   UUID                     NOT NULL REFERENCES channels (id) ON DELETE CASCADE,
+    last_read_at TIMESTAMPTZ              NOT NULL,
+    UNIQUE (user_id, channel_id)
+);
+
+CREATE TABLE IF NOT EXISTS message_attachments
+(
+    id            UUID PRIMARY KEY,
+    message_id    UUID NOT NULL REFERENCES messages (id) ON DELETE CASCADE,
+    attachment_id UUID NOT NULL REFERENCES binary_contents (id) ON DELETE CASCADE
+);
